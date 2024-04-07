539363f5672eff7585307cd8fea4b30a94b458de
Felix Ogucha <fenicfelix@gmail.com>
2024-04-07 06:58:50 +0300

    updates config file

--- config/mpesa.php
+++ config/mpesa.php
@@ -5,38 +5,44 @@ return [
     'env' => env('MPESA_ENV', 'sandbox'),
 
     // Define mpesa credentials
-    'consumer_key' => env('MPESA_CONSUMER_KEY', 'DvfzVK7Ow3SVkkSNAXZ8ETLmjAjpyMEo'),
-    'consumer_secret' => env('MPESA_CONSUMER_SECRET', '9szoKDVCHpAenjq7'),
+    'consumer_key' => env('MPESA_CONSUMER_KEY'),
+    'consumer_secret' => env('MPESA_CONSUMER_SECRET'),
 
     // Define mpesa shortcode
     'shortcode' => env('MPESA_SHORTCODE'),
 
-    // 'lipa_na_mpesa_online_passkey' => env('LIPA_NA_MPESA_ONLINE_PASSKEY'),
-    // 'initiator_name' => env('MPESA_INITIATOR_NAME'),
-    // 'initiator_password' => env('MPESA_INITIATOR_PASSWORD'),
-    // 'security_credential' => env('MPESA_SECURITY_CREDENTIAL'),
-    // 'validation_url' => env('MPESA_VALIDATION_URL'),
-    // 'confirmation_url' => env('MPESA_CONFIRMATION_URL'),
-    // 'stk_push_url' => env('MPESA_STK_PUSH_URL'),
-    // 'stk_push_query_url' => env('MPESA_STK_PUSH_QUERY_URL'),
-    // 'c2b_register_url' => env('MPESA_C2B_REGISTER_URL'),
-    // 'c2b_simulate_url' => env('MPESA_C2B_SIMULATE_URL'),
-    // 'account_balance_url' => env('MPESA_ACCOUNT_BALANCE_URL'),
-    // 'transaction_status_url' => env('MPESA_TRANSACTION_STATUS_URL'),
-    // 'reversal_url' => env('MPESA_REVERSAL_URL'),
-    // 'timeout_url' => env('MPESA_TIMEOUT_URL'),
+    // ------------- c2b configs -------------
+    'stk_passkey' => env('MPESA_PASSKEY'),
+    'initiator_name' => env('MPESA_INITIATOR_NAME'),
+    'initiator_password' => env('MPESA_INITIATOR_PASSWORD'),
+    'stk_validation_url' => env('MPESA_STK_VALIDATION_URL'),
+    'stk_confirmation_url' => env('MPESA_STK_CONFIRMATION_URL'),
+    'stk_callback_url' => env('MPESA_STK_CALLBACK_URL'),
 
-    // 'b2c_timeout_url' => env('MPESA_B2C_TIMEOUT_URL'),
-    // 'b2c_result_url' => env('MPESA_B2C_RESULT_URL'),
-    // 'b2b_timeout_url' => env('MPESA_B2B_TIMEOUT_URL'),
-    // 'b2b_result_url' => env('MPESA_B2B_RESULT_URL'),
-    // 'queue_timeout_url' => env('MPESA_QUEUE_TIMEOUT_URL'),
-    // 'queue_result_url' => env('MPESA_QUEUE_RESULT_URL'),
-    // 'stk_push_timeout_url' => env('MPESA_STK_PUSH_TIMEOUT_URL'),
-    // 'stk_push_result_url' => env('MPESA_STK_PUSH_RESULT_URL'),
-    // 'transaction_query_url' => env('MPESA_TRANSACTION_QUERY_URL'),
-    // 'reversal_timeout_url' => env('MPESA_REVERSAL_TIMEOUT_URL'),
-    // 'reversal_result_url' => env('MPESA_REVERSAL_RESULT_URL'),
-    // 'encryption_key' => env('MPESA_ENCRYPTION_KEY'),
-    // 'public_key' => env('MPESA_PUBLIC_KEY'),
+    // ------------- c2b reversal urls -------------
+    'reversal_timeout_url' => env('MPESA_REVERSAL_TIMEOUT_URL'),
+    'reversal_result_url' => env('MPESA_REVERSAL_RESULT_URL'),
+
+    // ------------- balance -------------
+    'balance_result_url' => env('MPESA_BALANCE_RESULT_URL'),
+    'balance_timeout_url' => env('MPESA_BALANCE_TIMEOUT_URL'),
+
+    // ------------- Transaction status urls -------------
+    'transaction_status_result_url' => env('MPESA_TRANSACTION_STATUS_RESULT_URL'),
+    'transaction_status_timeout_url' => env('MPESA_TRANSACTION_STATUS_TIMEOUT_URL'),
+
+    // ------------- b2c urls -------------
+    'b2c_timeout_url' => env('MPESA_B2C_TIMEOUT_URL'),
+    'b2c_result_url' => env('MPESA_B2C_RESULT_URL'),
+
+    // ------------- b2b urls -------------
+    'b2b_timeout_url' => env('MPESA_B2B_TIMEOUT_URL'),
+    'b2b_result_url' => env('MPESA_B2B_RESULT_URL'),
+
+    // ------------- bill manager optin urls -------------
+    'bill_optin_callback_url' => env('MPESA_BILL_OPTIN_CALLBACK_URL'),
+
+    // ------------- tax remmitance urls -------------
+    'tax_remittance_timeout_url' => env('MPESA_TAX_REMITTANCE_TIMEOUT_URL'),
+    'tax_remittance_result_url' => env('MPESA_TAX_REMITTANCE_RESULT_URL'),
 ];
